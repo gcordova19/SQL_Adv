dca46a615511b6acee02f9372026e85418acf329
Gabriela Cordova <gabriela.cordova@upc.edu>
2023-09-10 12:39:20 +0200

    solucion ejercicio2

--- README.md
+++ README.md
@@ -2,4 +2,12 @@
 
 - CREAR TABLA DE ivr_detail ->  ivr_detail.sql
 - CREAR TABLA DE ivr_summary -> ivr_summary.sql funcion extra vdn_aggregation.sql
+Modificacion:
+Este código realiza una serie de transformaciones y cálculos en los datos de llamadas telefónicas en una tabla llamada `ivr_detail` y luego crea una nueva tabla llamada `ivr_summary`. 
+
+1. Extrae datos relevantes de la tabla `ivr_detail`, como números de teléfono, fechas, duración de llamadas, etc.
+2. Asigna una categoría (`vdn_aggregation`) según ciertos patrones en la etiqueta de la llamada.
+3. Rellena valores nulos en las columnas de documentos con 'DESCONOCIDO'.
+4. Realiza cálculos para determinar si un número repite llamadas en las 24 horas siguientes (`repeated_phone_24H`) o si causa una llamada en las 24 horas posteriores (`cause_recall_phone_24H`).Esto ayuda a identificar patrones de llamadas repetidas en un corto período de tiempo.
+5. Agrupa los resultados por varios atributos y crea la tabla `ivr_summary` con los resultados de los cálculos y transformaciones anteriores.
 - CREAR FUNCIÓN DE LIMPIEZA DE ENTEROS -> EJERCICIO3.SQL
--- ivr_summary.sql
+++ ivr_summary.sql
@@ -1,78 +1,68 @@
 CREATE OR REPLACE TABLE keepcoding.ivr_summary AS
-WITH summary --base de clientes
-  AS (SELECT  calls_ivr_id AS ivr_id, 
-              calls_phone_number AS phone_number,
-              calls_ivr_result AS ivr_result,
-              keepcoding.vdn_aggregation(calls_vdn_label) AS vdn_aggregation,
-              calls_start_date AS start_date,
-              calls_start_date_id,
-              calls_end_date AS end_date,
-              calls_total_duration AS total_duration,
-              calls_customer_segment AS customer_segment,
-              calls_ivr_language AS ivr_language,
-              calls_steps_module AS steps_module,
-              calls_module_aggregation AS module_aggregation,
-              document_type, 
-              IF(document_type ='NULL', 0, 1) AS doc_type_lg,
-              document_identification, 
-              IF(document_identification ='NULL', 0, 1) AS doc_id_lg,
-              customer_phone, 
-              IF(customer_phone ='NULL', 0, 1) AS cust_phone_lg,
-              billing_account_id, 
-              IF(billing_account_id ='NULL', 0, 1) AS bill_lg,
-              IF(calls_module_aggregation LIKE '%AVERIA_MASIVA%', 1, 0) AS masiva_lg,
-              step_name, 
-              step_description_error,
-              IF(step_name = 'CUSTOMERINFOBYPHONE.TX' AND step_description_error = 'NULL', 1, 0) AS info_by_phone_lg,
-              IF(step_name = 'CUSTOMERINFOBYDNI.TX' AND step_description_error = 'NULL', 1, 0) AS info_by_dni_lg,
-              -- falta flag repeated_phone_24H
-              -- falta flag cause_recall_phone_24H
-      FROM keepcoding.ivr_detail summary 
-   QUALIFY ROW_NUMBER() OVER(PARTITION BY SAFE_CAST(ivr_id AS STRING) ORDER BY doc_type_lg DESC, doc_id_lg DESC, cust_phone_lg DESC, bill_lg DESC) = 1
-           ),
-llamadas 
-  AS (SELECT  phone_number,
-              IF(COUNT(phone_number) > 1, 1, 0) AS repeated_phone_24H,
-              COUNT(phone_number) AS rep_num,
-      FROM summary
-      WHERE summary.start_date >= DATE_SUB(summary.start_date , INTERVAL 1 DAY) 
-      GROUP BY phone_number
-      ORDER BY 1),
-llamadas_posterior
-  AS (SELECT  phone_number,
-              IF(COUNT(phone_number) > 1, 1, 0) AS cause_recall_phone_24H,
-      FROM summary
-      WHERE summary.start_date <= DATE_ADD(summary.start_date , INTERVAL 1 DAY) 
-      GROUP BY phone_number
-      ORDER BY 1)
-
-SELECT suma.ivr_id,
-      suma.phone_number,
-      suma.ivr_result,
-      suma.vdn_aggregation,
-      suma.start_date,
-      suma.end_date,
-      suma.total_duration,
-      suma.customer_segment,
-      suma.ivr_language,
-      suma.steps_module,
-      suma.module_aggregation,
-      suma.document_type,
-      suma.document_identification,
-      suma.customer_phone, 
-      suma.billing_account_id,
-      suma.masiva_lg,
-      suma.info_by_phone_lg,
-      suma.info_by_dni_lg,
-      ll.repeated_phone_24H,
-      lp.cause_recall_phone_24H
-  FROM summary suma
-  LEFT
-  JOIN llamadas ll
-    ON suma.phone_number = ll.phone_number
-
-  LEFT
-  JOIN llamadas_posterior lp
-    ON suma.phone_number = lp.phone_number
-
-  
+WITH Temp AS (
+  SELECT
+    calls_ivr_id AS ivr_id,
+    calls_phone_number AS phone_number,
+    calls_ivr_result AS ivr_result,
+    CASE
+      WHEN calls_vdn_label LIKE 'ATC%' THEN 'FRONT'
+      WHEN calls_vdn_label LIKE 'TECH%' THEN 'TECH'
+      WHEN calls_vdn_label = 'ABSORPTION' THEN 'ABSORPTION'
+      ELSE 'RESTO'
+    END AS vdn_aggregation,
+    calls_start_date AS start_date,
+    calls_end_date AS end_date,
+    calls_total_duration AS total_duration,
+    calls_customer_segment AS customer_segment,
+    calls_ivr_language AS ivr_language,
+    calls_steps_module AS steps_module,
+    calls_module_aggregation AS module_aggregation,
+    document_type,
+    document_identification,
+    customer_phone,
+    billing_account_id,
+    CAST(calls_steps_module AS STRING) AS str_steps_module,
+    calls_start_date AS start_date_repeated,
+    -- Acceder al valor de una columna de la fila anterior dentro del conjunto de resultados ordenado
+    LAG(calls_start_date) OVER (PARTITION BY calls_phone_number ORDER BY calls_start_date) AS lag_start_date,
+    -- Acceder al valor de una columna de la fila siguiente dentro del conjunto de resultados ordenado
+    LEAD(calls_start_date) OVER (PARTITION BY calls_phone_number ORDER BY calls_start_date) AS lead_start_date,
+    step_description_error
+  FROM `entrega-378209.keepcoding.ivr_detail`
+)
+SELECT
+  ivr_id,
+  phone_number,
+  ivr_result,
+  vdn_aggregation,
+  start_date,
+  end_date,
+  total_duration,
+  customer_segment,
+  ivr_language,
+  steps_module,
+  module_aggregation,
+  COALESCE(document_type, 'DESCONOCIDO') AS document_type,
+  COALESCE(document_identification, 'DESCONOCIDO') AS document_identification,  
+  IFNULL(MAX(NULLIF(customer_phone, 'NULL')), 'DESCONOCIDO') AS customer_phone,
+  COALESCE(MAX(NULLIF(billing_account_id, 'NULL')), 'DESCONOCIDO') AS billing_account_id,
+  MAX(CASE WHEN str_steps_module LIKE '%AVERIA_MASIVA%' THEN 1 ELSE 0 END) AS masiva_lg,
+  MAX(CASE WHEN str_steps_module LIKE '%CUSTOMERINFOBYPHONE.TX%' AND step_description_error IS NULL THEN 1 ELSE 0 END) AS info_by_phone_lg,
+  MAX(CASE WHEN str_steps_module LIKE '%CUSTOMERINFOBYDNI.TX%' AND step_description_error IS NULL THEN 1 ELSE 0 END) AS info_by_dni_lg,
+  MAX(CASE WHEN TIMESTAMP_DIFF(start_date, lag_start_date, HOUR) < 24 THEN 1 ELSE 0 END) AS repeated_phone_24H,
+  MAX(CASE WHEN TIMESTAMP_DIFF(lead_start_date, end_date, HOUR) < 24 THEN 1 ELSE 0 END) AS cause_recall_phone_24H
+FROM Temp
+GROUP BY
+  ivr_id,
+  phone_number,
+  ivr_result,
+  vdn_aggregation,
+  start_date,
+  end_date,
+  total_duration,
+  customer_segment,
+  ivr_language,
+  steps_module,
+  module_aggregation,
+  document_type,
+  document_identification;
